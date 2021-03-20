1fde7d5f0c05b2b6f4295019d269558052ea290d
pmaharjan <pratikmaharjan88@gmail.com>
2021-03-20 19:36:28 -0400

    Web instances and elastic ip

--- prod.tf
+++ prod.tf
@@ -38,5 +38,23 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
+resource "aws_instance" "prod_web" {
+  ami           = "ami-02057812be096119e"
+  instance_type = "t2.nano"
 
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
 
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
